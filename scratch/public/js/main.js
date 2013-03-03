97698f27a4e9e13af12b09ee7e813896a9f5c76a
Arthur Chang <halcyonbeing@gmail.com>
2013-03-03 17:31:42 -0500

    Nut fix

--- scratch/public/js/main.js
+++ scratch/public/js/main.js
@@ -1,6 +1,7 @@
 /* Arthur Chang: arthurc
  */
 window.onready = function(){
+#("#nut").hide();
     $(".outter_box").click(function(){
         var unchecked = $(this).children(".device_box").children("div .uncheck");
         var checked = $(this).children(".device_box").children("div .check");
@@ -10,6 +11,14 @@ window.onready = function(){
         if(checked !=undefined){
             checked.removeClass("check").addClass("uncheck");
         }
+    });
+
+    $(".outter_box").mouseon(function(){
+        $("#nut").show();
+    });
+
+    $(".outter_box").mouseoff(function(){
+        $("#nut").hide();
     });
 }
 
